02b26defac44c653694a6b759830646927a26dad
Anton Mykhaylov <amykhaylov@antons-mbp.lan>
2018-01-21 14:41:37 -0600

    cleanup

--- recipeAPI.js
+++ recipeAPI.js
@@ -13,12 +13,12 @@
                 console.log(response);
 
                 // Google places API
-                $.ajax({
-                    url: "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIzaSyDSYIY3_YzWDSDew4jVIB5TVYty8-wESxA",
-                    method: "GET"
-                }).done(function(response) {
-                    console.log(response);
-                });
+                // $.ajax({
+                //     url: "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIzaSyDSYIY3_YzWDSDew4jVIB5TVYty8-wESxA",
+                //     method: "GET"
+                // }).done(function(response) {
+                //     console.log(response);
+                // });
 
                 // Storing the data from the AJAX request in the results variable
                 var results = response.hits;
@@ -72,74 +72,4 @@
         function activatePlacesSearch() {
             var input = document.getElementById('map-search');
             var autocomplete = new google.maps.places.Autocomplete(input);
-        }
-
-        // This example requires the Places library. Include the libraries=places
-        // parameter when you first load the API. For example:
-        // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
-        var infoWindow;
-        var map;
-        var service;
-
-        function initMap() {
-            map = new google.maps.Map(document.getElementById('map'), {
-                center: { lat: -33.867, lng: 151.206 },
-                zoom: 15,
-                styles: [{
-                    stylers: [{ visibility: 'simplified' }]
-                }, {
-                    elementType: 'labels',
-                    stylers: [{ visibility: 'off' }]
-                }]
-            });
-
-            infoWindow = new google.maps.InfoWindow();
-            service = new google.maps.places.PlacesService(map);
-
-            // The idle event is a debounced event, so we can query & listen without
-            // throwing too many requests at the server.
-            map.addListener('idle', performSearch);
-        }
-
-        function performSearch() {
-            var request = {
-                bounds: map.getBounds(),
-                keyword: 'best view'
-            };
-            service.radarSearch(request, callback);
-        }
-
-        function callback(results, status) {
-            if (status !== google.maps.places.PlacesServiceStatus.OK) {
-                console.error(status);
-                return;
-            }
-            for (var i = 0, result; result = results[i]; i++) {
-                addMarker(result);
-            }
-        }
-
-        function addMarker(place) {
-            var marker = new google.maps.Marker({
-                map: map,
-                position: place.geometry.location,
-                icon: {
-                    url: 'https://developers.google.com/maps/documentation/javascript/images/circle.png',
-                    anchor: new google.maps.Point(10, 10),
-                    scaledSize: new google.maps.Size(10, 17)
-                }
-            });
-
-            google.maps.event.addListener(marker, 'click', function() {
-                service.getDetails(place, function(result, status) {
-                    if (status !== google.maps.places.PlacesServiceStatus.OK) {
-                        console.error(status);
-                        return;
-                    }
-                    infoWindow.setContent(result.name);
-                    infoWindow.open(map, marker);
-                });
-            });
-            console.log(infoWindow);
         }
