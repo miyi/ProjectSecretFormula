864ff4b0a21fdf289a9b88464db06a3993d7a99f
Anton Mykhaylov <amykhaylov@antons-mbp.lan>
2018-01-21 11:45:55 -0600

    Added Google Places search functionality

--- recipeAPI.js
+++ recipeAPI.js
@@ -68,118 +68,78 @@
             event.preventDefault();
         });
 
-        var map;
-        var infowindow;
-        var restaurants = [];
-
-        var restaurantName;
-        var restaurantAddress;
-        var restaurantRating;
-        var restaurantPrice;
-
-        var groupName;
-        var groupDate;
-        var groupParticipants;
-        var groupTime;
-        var groupTheme;
-
-
-
-
-        $(".overlay").hide();
-
-        function initMap() {
-            var pyrmont = { lat: 41.896294, lng: -87.618799 };
-
-            map = new google.maps.Map(document.getElementById('map'), {
-                center: pyrmont,
-                zoom: 15
-            });
-
-            infowindow = new google.maps.InfoWindow();
-            var service = new google.maps.places.PlacesService(map);
-            service.nearbySearch({
-                location: pyrmont,
-                radius: 800,
-                type: ['restaurant']
-            }, callback);
+        // Google Places search functionality
+        function activatePlacesSearch() {
+            var input = document.getElementById('map-search');
+            var autocomplete = new google.maps.places.Autocomplete(input);
         }
 
-        function callback(results, status) {
-            if (status === google.maps.places.PlacesServiceStatus.OK) {
-                for (var i = 0; i < results.length; i++) {
-                    var label0 = i + 1;
-                    var label = label0.toString();
-                    var id = "restaurant" + label;
+        // This example requires the Places library. Include the libraries=places
+        // parameter when you first load the API. For example:
+        // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 
-                    createMarker(results[i], label);
+        // var map;
+        // var infoWindow;
+        // var service;
 
-                    $("#well").append(
-                        "<h2>" + label + ": " + results[i].name + "</h2>" +
-                        "<p><strong>Rating: </strong>" + results[i].rating + "</p>" +
-                        "<p><strong>Price Level: </strong>" + results[i].price_level + "</p>" +
-                        "<p><strong>Address: </strong>" + results[i].vicinity + "</p>" +
-                        "<button id='" + id + "'>Select Restaurant</button>" +
-                        "<br>");
+        // function initMap() {
+        //     map = new google.maps.Map(document.getElementById('map'), {
+        //         center: { lat: -33.867, lng: 151.206 },
+        //         zoom: 15,
+        //         styles: [{
+        //             stylers: [{ visibility: 'simplified' }]
+        //         }, {
+        //             elementType: 'labels',
+        //             stylers: [{ visibility: 'off' }]
+        //         }]
+        //     });
 
-                    createClickEvent(id, i);
+        //     infoWindow = new google.maps.InfoWindow();
+        //     service = new google.maps.places.PlacesService(map);
 
-                    updateRestaurantsList(results[i]);
-                }
-            }
-        }
+        //     // The idle event is a debounced event, so we can query & listen without
+        //     // throwing too many requests at the server.
+        //     map.addListener('idle', performSearch);
+        // }
 
-        function createMarker(place, label) {
-            var placeLoc = place.geometry.location;
-            var marker = new google.maps.Marker({
-                map: map,
-                position: place.geometry.location,
-                label: label
-            });
+        // function performSearch() {
+        //     var request = {
+        //         bounds: map.getBounds(),
+        //         keyword: 'best view'
+        //     };
+        //     service.radarSearch(request, callback);
+        // }
 
-            google.maps.event.addListener(marker, 'click', function() {
-                infowindow.setContent(place.name);
-                infowindow.open(map, this);
-            });
-        }
+        // function callback(results, status) {
+        //     if (status !== google.maps.places.PlacesServiceStatus.OK) {
+        //         console.error(status);
+        //         return;
+        //     }
+        //     for (var i = 0, result; result = results[i]; i++) {
+        //         addMarker(result);
+        //     }
+        // }
 
-        function createClickEvent(id, i) {
-            $("#" + id).on("click", function() {
-                $(".overlay").show();
+        // function addMarker(place) {
+        //     var marker = new google.maps.Marker({
+        //         map: map,
+        //         position: place.geometry.location,
+        //         icon: {
+        //             url: 'https://developers.google.com/maps/documentation/javascript/images/circle.png',
+        //             anchor: new google.maps.Point(10, 10),
+        //             scaledSize: new google.maps.Size(10, 17)
+        //         }
+        //     });
 
-                restaurantName = restaurants[i].name;
-                restaurantAddress = restaurants[i].address;
-                restaurantRating = restaurants[i].rating;
-                restaurantPrice = restaurants[i].price;
-
-                console.log(restaurantName, restaurantAddress, restaurantRating, restaurantPrice);
-
-            });
-        }
-
-
-        function updateRestaurantsList(results) {
-            restaurants.push({
-                "name": results.name,
-                "rating": results.rating,
-                "price": results.price_level,
-                "address": results.vicinity
-            });
-        }
-
-
-        $("#createGroupBtn").on("click", function(event) {
-
-            event.preventDefault();
-
-            groupName = $("#groupName").val();
-            groupDate = $("#groupDate").val();
-            groupParticipants = $("#groupParticipants").val();
-            groupTime = $("#groupTime").val();
-            groupTheme = $("#groupTheme").val();
-
-            // window.location.href = "myGroups.html";
-
-            console.log(groupName, groupDate, groupParticipants, groupTime, groupTheme);
-
-        });
+        //     google.maps.event.addListener(marker, 'click', function() {
+        //         service.getDetails(place, function(result, status) {
+        //             if (status !== google.maps.places.PlacesServiceStatus.OK) {
+        //                 console.error(status);
+        //                 return;
+        //             }
+        //             infoWindow.setContent(result.name);
+        //             infoWindow.open(map, marker);
+        //         });
+        //     });
+        //     console.log(infoWindow);
+        // }
